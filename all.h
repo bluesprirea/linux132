e64c400c62cd6db7cee3e55df0a95171250141c8
chiho <chiho@chiho-VirtualBox.(none)>
2013-07-30 01:05:37 +0900

    3rd assignment

---
+++ all.h
@@ -0,0 +1,8 @@
+#ifndef ALL_H
+#define ALL_H
+
+double power(double a, int b);
+void printBye();
+void printHello();
+
+#endif
---
+++ power.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+double power(double a, int b) {
+	double ret = 1.0;
+	int i;
+	for (i=0 ; i <b ; i++)
+		ret *= a;
+	return ret;
+}
